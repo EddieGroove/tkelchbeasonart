1cf1f5ddbc8ffb86b2dbadf51e3752c6d6f98ee3
Clayton Ramer <clayton.ramer@gmail.com>
2023-10-14 10:49:19 -0400

    Fix path file bug in admin feature

--- addpage.ps1
+++ addpage.ps1
@@ -2,11 +2,20 @@
 $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
 $Image = $FileBrowser.ShowDialog()
 
+$Path = 'C:\Users\clayt\tkelchbeasonart'
 $Artwork = Read-Host -Prompt 'Name your piece of artwork'
 $Size = Read-Host -Prompt 'Enter the size in format (9 x 11)'
 $Medium = Read-Host -Prompt 'Enter medium e.g. copic market, gel pen'
 
 $PageName = $Artwork -replace '\s','_'
-Move-Item -Path $FileBrowser.FileName -Destination C:\Users\clayt\tkelchbeasonart\artwork\$PageName.jpg
+Move-Item -Path $FileBrowser.FileName -Destination $Path\artwork\$PageName.jpg
 
-New-Item .\tkelchbeasonart\pages\$PageName.html
+Copy-Item -Path $Path\pages\template.html -Destination $Path\pages\$PageName.html
+
+$PageContent = Get-Content -Path $Path\pages\$PageName.html
+$PageContent = $PageContent -replace 'artworkName', $Artwork
+$PageContent = $PageContent -replace 'pictureName', $PageName
+$PageContent = $PageContent -replace 'size', $Size
+$PageContent = $PageContent -replace 'medium', $Medium
+
+$PageContent | Set-Content -Path $Path\pages\$PageName.html
